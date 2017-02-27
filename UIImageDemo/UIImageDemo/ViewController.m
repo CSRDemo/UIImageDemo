ec75f282f107c4fdc22aba9be4a4102613d341d9
LeoAiolia <a9363818@163.com>
2017-02-27 21:40:29 +0800

    init

---
+++ UIImageDemo/UIImageDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  UIImageDemo
+//
+//  Created by User on 2017/2/27.
+//  Copyright © 2017年 LeoAiolia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
